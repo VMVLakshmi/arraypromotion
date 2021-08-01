be5ca7e58b8e8eedb4e8743f9f429e342d2c4a71
hai <hai@LAPTOP-4DV9N6FJ>
2021-08-01 19:35:58 +0530

    Ninth commit
    Upload all files

---
+++ ARRAY_PROMOTION/src/datatypes/Promotion.java
@@ -0,0 +1,15 @@
+package datatypes;
+
+public class Promotion {
+
+	public static void main(String[] args) {
+		byte a=40;
+		 byte b=50;
+		 byte c=100;
+		 int d=a*b/c;
+		 System.out.println(d);
+		 
+
+	}
+
+}
